51136d0ed519d34887301651965ae6371d22b015
SumanDas3001 <sumandas3001@gmail.com>
2019-07-02 10:40:19 +0530

    Create scaffold for post

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # model association
+  has_many :posts, dependent: :destroy
+
+  validates_presence_of :title, :description, :content, :author_name
 end
